d7bbde3567afd98f778e087643f09ba7601b1840
Jhon Jairo Cardenas <jjcardenasan@unadvirtual.edu.co>
2026-05-08 16:50:28 -0500

    codigo servicio alquiler de equipos

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -69,3 +69,25 @@ class Servicio(ABC):
     @abstractmethod
     def describir_servicio(self):
         pass
+    # aporte jhon jairo cardenas
+    # creamos el servicio para alquilar salas. como es hijo de servicio ya sabe que tiene un nombre y un precio base
+class ReservaSalas(Servicio):
+    def describir_servicio(self): # esta linea sirve para cuando el programa pregunte que es esto responda sala de juntas
+        return f"sala de juntas ({self.tipo})" #seguido del tipo de sala
+    # se define como cobrar
+    def calcular_costo(self, horas, descuento=0):
+     
+        if horas > 12: # pongo un limite si el cliente se pasa de 12 horas el programa se detiene
+            raise ErrorDeNegocio("parametro excedido maximo 12 horas por reserva")#lanzo el aviso de error explicando que no se puede reservar tantas horas
+        total = (self.precio_base*horas) - descuento #operacion matematica multiplico el precio por el tiempo y le resto el descuento
+        return max(total,0)# entrego el resultado final asegurandome de que si el descuento es mayor al precio el sistema no me de numeros negativos
+# Esta es la segunda clase tambien es hija de servicio aqui alquilamos objetos fisicos
+class AlquilerEquipos(Servicio):
+    def describir_servicio(self):#identifico el servicio como un equipo tecnologico y lo muestro en pantalla
+        return f"Equipo Tecnologico ({self.tipo})"
+    def calcular_costo(self, dias, tiene_seguro=True): #pedimos los dias y preguntamos si el cliente tiene seguro
+        cargo_seguro = 15000 if tiene_seguro else 0 # si tiene seguro le sumamos 15000 si no le sumamos 0
+        return (self.precio_base*dias) + cargo_seguro # devuelvo el cobro final sumando el precio por los dias mas el seguro
+    
+
+  
