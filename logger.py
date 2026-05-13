76b3d0f03758e1b162f08dc71f9f25cccb0c81d0
Jhon Jairo Cardenas Anteliz <jjcardenasan@unadvirtual.edu.co>
2026-05-12 19:09:50 -0500

    Corrección de nombre de archivo log en comentarios

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import logging # Importamos la libreria para llevar el registro de lo que pasa en el sistema
 #  Registro de errores y eventos 
-# en el archivo errores_programa.txt
+# en el archivo errores_programa
 logging.basicConfig(
     filename='errores_programa.log', # Nombre del archivo donde se guardará todo
     level=logging.INFO, # registra cada movimiento importante
